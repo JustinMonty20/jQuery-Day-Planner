ab8c9f74cdc017987c353c812ac7541915a8a2ff
Justin Montgomery <montgomerycodepro@gmail.com>
2020-02-09 13:20:58 -0500

    generating the web-page html with jQuery

--- assets/day-planner.js
+++ assets/day-planner.js
@@ -1,19 +1,29 @@
 var now = (moment().format("dddd, MMM Do"));
 
-var saveBtn = $("<button class = 'saveBtn'><span><i class='fas fa-save icon-large'></i></span></button>");
+var saveBtn = $();
 
 $("#currentDay").html(now);
 
 
 
 for (i = 0; i < 9; i++) {
-   var timeBlocks = $("<div class = 'time-block row'></div>")
-
-   $(".container").append(timeBlocks);
-
+    var timeBlocks = $("<div class = 'time-block row'></div>");
+    var saveBtn = $("<button class = 'saveBtn'><span><i class='fas fa-save icon-large'></i></span></button>")
+    var timeDisplay = $("<div class ='hour'></div>");
+    var textArea = $("<textarea> </textarea>");
+    
+    // adding bootstrap classes to the specific elements. 
+    timeDisplay.addClass("col-md-2");
+    textArea.addClass("col-md-8");
+    saveBtn.addClass("col-md-2");
+  
+  
+    
+    $(".container").append(timeBlocks);
+    timeBlocks.prepend(saveBtn);
+    timeBlocks.prepend(textArea);
+    timeBlocks.prepend(timeDisplay);
 
 }
-$("div").each(function(i){
-    var saveBtn = $("<button class = 'saveBtn'><span><i class='fas fa-save icon-large'></i></span></button>");
 
-})
+console.log(timeBlocks)
